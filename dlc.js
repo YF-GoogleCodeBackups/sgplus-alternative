0d6ca734eadb6f61465c4fbf66b7ea7323a41e0c
dotazured <dotazured@gmail.com>
2013-11-07 15:55:18 +0000

    Added as DLC:
    Forge - Starter Pack
    Guardians of Middle-earth: The Warrior Bundle
    Guardians of Middle-earth: Smaug's Treasure
    Guardians of Middle-earth: The Company of Dwarves Bundle

--- dlc.js
+++ dlc.js
@@ -2305,4 +2305,12 @@ postMessage({dlc:{
 		"Magic 2012 Full Deck \u201cBeknighted\u201d  ", 
 		"Magic 2012 Full Deck \u201cTrinity of Elements\u201d "
 	]
+	"Guardians of Middle-earth": [
+		"Guardians of Middle-earth: The Company of Dwarves Bundle",
+		"Guardians of Middle-earth: Smaug's Treasure",
+		"Guardians of Middle-earth: The Warrior Bundle"
+	]
+	"Forge Free to Play": [
+		"Forge - Starter Pack"
+	]
 }},'*')
